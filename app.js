c211051e4f1309bde64d1afbd8c5bf04d1f879da
JohannGon118 <johann_gonzales@dlsu.edu.ph>
2020-04-12 12:58:18 +0800

    Completed App

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const indexRouter = require('./routes/index');
 const app = express();
 const port = 9090;
 
+const session = require('express-session');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo')(session);
+
 // Listening to the port provided
 app.listen(port, () => {
   console.log('App listening at port ' + port)
@@ -41,6 +45,22 @@ app.use(express.static('public'));
 
 // Insert server configuration after this comment
 
+app.use(session({
+  secret: 'somegibberishsecret',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
+
+app.use(flash());
+
+// Global messages vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 app.use('/', authRouter); // Login/registration routes
 app.use('/', indexRouter); // Main index route
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-
-router.get('/', (req, res) => {
+const { isPrivate } = require('../middlewares/checkAuth');
+router.get('/', isPrivate, (req, res) => {
   res.render('home', { pageTitle: 'Home', name: req.session.name } );
 });
 
